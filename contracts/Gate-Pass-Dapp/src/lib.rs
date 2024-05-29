04b05b167caa8841f8c26a5ee19284f827b3f88e
Bhupendra <iiitianbhupendra@gmail.com>
2024-05-29 11:52:03 +0530

    Smartcontract Update: 1) Pass of users is now mapped to their address. 2) Every user can now only have one pass at a time linked to their adderss and will will only be able to create a new pass when their previous one expires. 3) Added authentication to functions. 4) The admin can now also see the count of expired passes as well.

--- contracts/Gate-Pass-Dapp/src/lib.rs
+++ contracts/Gate-Pass-Dapp/src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 #![no_std]
-use soroban_sdk::{contract, contracttype, contractimpl, log, Env, Symbol, String, symbol_short};
+use soroban_sdk::{Address, contract, contracttype, contractimpl, log, Env, Symbol, String, symbol_short};
 
 
 // Created a ApprovalStatus structure and defined all the important parameters that I want to track for a Pass.
@@ -9,6 +9,7 @@ use soroban_sdk::{contract, contracttype, contractimpl, log, Env, Symbol, String
 pub struct ApprovalStatus {
     pub approved: u64, // for storing the 'Count of Approved Passes' 
     pub pending: u64,  // for storing the 'Count of Passes pending for approval'
+    pub expired: u64,     // for storing the 'Count of Expired passes on our platform or dApp'
     pub total: u64     // for storing the 'Count of Total pass made on our platform or dApp'
 }
 
@@ -20,7 +21,7 @@ const ALL_PASS : Symbol = symbol_short!("ALL_PASS");
 // Mapping Pass to, its Pass-Id
 #[contracttype] 
 pub enum Passbook { 
-    Pass(u64)
+    Pass(Address)
 }
 
 
@@ -32,8 +33,6 @@ const COUNT_PASS: Symbol = symbol_short!("C_PASS");
 #[contracttype]
 #[derive(Clone)] 
 pub struct Pass {
-    pub name: String, 
-    pub record_id: u64,
     pub title: String,
     pub descrip: String,
     pub out_time: u64, 
@@ -50,43 +49,53 @@ pub struct GatePassContract;
 impl GatePassContract {
 
     // This function creates and retuns a new pass:
-    pub fn create_pass(env: Env, user: String, title: String, descrip: String) {
-        let mut count_pass: u64 = env.storage().instance().get(&COUNT_PASS).unwrap_or(0);
-            count_pass += 1;
-        // log!(&env, "count pass: {}", count_pass);
+    pub fn create_pass(env: Env, record_id: Address, title: String, descrip: String) -> Symbol {
+        // authenticating user.
+        record_id.require_auth();
         
         // indirectly creating an instance of 'Record' struct.
-        let mut records = Self::view_my_pass(env.clone(), count_pass.clone());   
+        let mut records = Self::view_my_pass(env.clone(), record_id.clone());   
 
-        let time = env.ledger().timestamp();
-
-        let mut all_pass = Self::view_all_pass_status(env.clone()); 
-
-            // recording the pass data
-            records.record_id = count_pass.clone();
-            records.name = user.clone();
-            records.title = title;
-            records.descrip = descrip;
-            records.out_time = time;
-        
-            // if the pass is created but not approved, then we will increase the pending approval count 
-            if records.approval == false {
+        // making sure that not more than 1 pass is assigned to an record_id(or account):
+        if records.isexpired == true {
+            let mut count_pass: u64 = env.storage().instance().get(&COUNT_PASS).unwrap_or(0);
+                count_pass += 1;
+            // log!(&env, "count pass: {}", count_pass);
+    
+            let time = env.ledger().timestamp();
+    
+            let mut all_pass = Self::view_all_pass_status(env.clone()); 
+    
+                // recording the pass data
+                records.title = title;
+                records.descrip = descrip;
+                records.out_time = time;
+                records.isexpired = false;
+            
                 all_pass.pending += 1;
                 all_pass.total = all_pass.total + 1;
-            } 
-            
-            // storing the newly created pass
-            env.storage().persistent().set(&Passbook::Pass(records.record_id.clone()), &records);
-            // updating the all_pass data
-            env.storage().persistent().set(&ALL_PASS, &all_pass);
-            // updating the count_pass
-            env.storage().instance().set(&COUNT_PASS, &count_pass);
-            log!(&env, "Pass ID {} Created. Wait for Approval!!", records.record_id);
+                
+                // storing the newly created pass
+                env.storage().instance().set(&Passbook::Pass(record_id.clone()), &records);
+                // updating the all_pass data
+                env.storage().instance().set(&ALL_PASS, &all_pass);
+                // updating the count_pass
+                env.storage().instance().set(&COUNT_PASS, &count_pass);
+    
+                env.storage().instance().extend_ttl(5000, 5000);
+                
+                return symbol_short!("Created");
+        } else {
+            panic!("You can't create a pass!");
+        }
     }
     
 
     // This function enable the admin to approve and updates the status of a newly registered pass.
-    pub fn approve_pass (env: Env, record_id: u64) {
+    pub fn approve_pass (env: Env, record_id: Address) {
+        // authenticating user.
+        record_id.require_auth();
+        
         let mut records = Self::view_my_pass(env.clone(), record_id.clone());   
         
         // If Pass is created but is not approved, then we'll set the approval-status of the pass to true
@@ -94,70 +103,89 @@ impl GatePassContract {
             records.approval = true;
 
             // Updating the Pass data
-            env.storage().persistent().set(&Passbook::Pass(records.record_id.clone()), &records);
+            env.storage().instance().set(&Passbook::Pass(record_id.clone()), &records);
 
             let mut all_pass = Self::view_all_pass_status(env.clone()); 
             all_pass.approved += 1; // increasing the approved pass count
-            all_pass.pending -= 1;  // decreasing the approved pass count
+            all_pass.pending -= 1;  // decreasing the pending pass count
 
             // Updating the Pass data
-            env.storage().persistent().set(&ALL_PASS, &all_pass);
+            env.storage().instance().set(&ALL_PASS, &all_pass);
             
-            log!(&env, "Pass ID {} is now Approved", record_id);
-        } else {
+            env.storage().instance().extend_ttl(5000, 5000);
 
-            panic!("Pass ID: {} is Already Approve!!", record_id);
+            log!(&env, "Your pass is now Approved");
+        } else {
+            log!(&env, "Cannot Approved!!");
+            panic!("Cannot Approved!!")
         } 
     }
 
     // This function gets triggered when the user wants to expire their pass:
-    pub fn expire_pass (env: Env, record_id: u64) {
+    pub fn expire_pass (env: Env, record_id: Address) {
+        // authenticating user.
+        record_id.require_auth();
+        
         let mut records = Self::view_my_pass(env.clone(), record_id.clone());   
 
         // Checking the Pass is whether approved or not:
-        if records.approval == true && records.in_time == 0 {
+        if records.approval == true && records.isexpired == false {
             let time = env.ledger().timestamp();
             
-            // If Pass is approved then we'll set the expiry-status of the pass to true
+            // If Pass is approved then we'll set the expiry-status of the pass to true and approval-status to false.
             records.isexpired = true;
+            records.approval = false;
             records.in_time = time;
+
+            let mut all_pass = Self::view_all_pass_status(env.clone());
+            all_pass.approved -= 1; // decreasing the approved pass count
+            all_pass.expired += 1; // increasing the expired pass count
             // Updating the Pass data
-            env.storage().persistent().set(&Passbook::Pass(records.record_id), &records);
+            env.storage().instance().set(&ALL_PASS, &all_pass);
             
-            log!(&env, "Pass ID {} is now Expired", record_id);
+            // Updating the Pass data
+            env.storage().instance().set(&Passbook::Pass(record_id.clone()), &records);
+            
+            env.storage().instance().extend_ttl(5000, 5000);
+            
+            log!(&env, "Your pass is now Expired");
         } else {
             // If Pass is does not exits or approved then we'll just terminate the function from execution.
-            panic!("Either Pass ID: {} doesn't exits, or is not approved yet!!", record_id);
+            log!(&env, "Either you haven't created your pass, or you pass is not approved yet!!");
+            panic!("Either you haven't created your pass, or you pass is not approved yet!!");
         } 
     }
 
     
     // This function gets triggered when the admin wants to get the status data of all registered pass:
-    pub fn view_all_pass_status (env: Env) -> ApprovalStatus {
-            // getting data related to all the registered passes. 
-            env.storage().instance().get(&ALL_PASS).unwrap_or(ApprovalStatus {
+    pub fn view_all_pass_status (env: Env) -> ApprovalStatus {    
+        // getting data related to all the registered passes. 
+        env.storage().instance().get(&ALL_PASS).unwrap_or(ApprovalStatus {
             // If data no found, then will return this ApprovalStatus object with default values: 
             approved: 0,
             pending: 0,
+            expired: 0,
             total: 0
         })
     }
     
     // This function gets triggered when the user wants to get the status data of their pass:
-    pub fn view_my_pass(env: Env, record_id: u64) -> Pass {
+    // pub fn view_my_pass(env: Env, record_id: u64) -> Pass {
+    pub fn view_my_pass(env: Env, record_id: Address) -> Pass {
+        // authenticating user.
+        record_id.require_auth();
+        
         let key = Passbook::Pass(record_id.clone()); 
         
         // getting data fron the blockchain for the 'key'
         env.storage().instance().get(&key).unwrap_or(Pass {
             // If data no found, then will return this Pass object with default values: 
-            name: String::from_str(&env, "Not_Found"),
-            record_id: 0,
             title: String::from_str(&env, "Not_Found"),
             descrip: String::from_str(&env, "Not_Found"),
             out_time: 0,
             in_time: 0,
             approval: false,
-            isexpired: false, 
+            isexpired: true, 
         })
     }
 }
